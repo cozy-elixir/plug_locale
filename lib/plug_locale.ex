80e19f6e6b397a19472eed0cb82734a17db8e7fc
c4710n <c4710n@users.noreply.github.com>
2023-12-28 12:07:57 +0800

    init: mix new plug_locale

---
+++ lib/plug_locale.ex
@@ -0,0 +1,18 @@
+defmodule PlugLocale do
+  @moduledoc """
+  Documentation for `PlugLocale`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugLocale.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plug_locale_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugLocaleTest do
+  use ExUnit.Case
+  doctest PlugLocale
+
+  test "greets the world" do
+    assert PlugLocale.hello() == :world
+  end
+end
