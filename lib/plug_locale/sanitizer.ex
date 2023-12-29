492f052c603759446eaca9b0d1289ed5ff8df7cb
c4710n <c4710n@users.noreply.github.com>
2023-12-29 20:27:12 +0800

    docs: polish docs for PlugLocale.Sanitizer

--- lib/plug_locale/sanitizer.ex
+++ lib/plug_locale/sanitizer.ex
@@ -1,29 +1,47 @@
 defmodule PlugLocale.Sanitizer do
   @moduledoc """
   The default implementation is a function like `fn x -> x end`, which does
-  nothing. But, in practice, you will need to set it to something meaningful.
+  nothing. But, in practice, you will need to use something meaningful.
 
-  For example:
+  A possible implementation:
 
-      def sanitize(locale) do
-        case locale do
-          # explicit matching on supported locales
-          locale when locale in ["en", "zh"] ->
-            locale
+      defmodule DemoWeb.LocaleSanitizer do
+        def sanitize(locale) do
+          case locale do
+            # explicit matching on supported locales
+            locale when locale in ["en", "zh"] ->
+              locale
 
-          # fuzzy matching on en locale
-          "en-" <> _ ->
-            "en"
+            # fuzzy matching on en locale
+            "en-" <> _ ->
+              "en"
 
-          # fuzzy matching on zh locale
-          "zh-" <> _ ->
-            "zh"
+            # fuzzy matching on zh locale
+            "zh-" <> _ ->
+              "zh"
 
-          # fallback for unsupported locales
-          _ ->
-            "en"
+            # fallback for unsupported locales
+            _ ->
+              "en"
+          end
         end
       end
+
+  Then, use above implementation in plugs by using `:sanitize_locale` option:
+
+      # use it for PlugLocale.WebBrowser
+      plug PlugLocale.WebBrowser,
+        default_locale: "en",
+        locales: ["en", "zh"],
+        sanitize_locale: &DemoWeb.LocaleSanitizer.sanitize/1,
+        # ...
+
+      # use it for PlugLocale.Header
+      plug PlugLocale.Header,
+        default_locale: "en",
+        locales: ["en", "zh"],
+        sanitize_locale: &DemoWeb.LocaleSanitizer.sanitize/1,
+        # ...
 
   """
 
