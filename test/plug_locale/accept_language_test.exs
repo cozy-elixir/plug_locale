668ed1cb8aabb370ca425473e014dfdcf501ebb8
c4710n <c4710n@users.noreply.github.com>
2023-12-29 10:26:11 +0800

    test: add tests for PlugLocale.AcceptLanguage

---
+++ test/plug_locale/accept_language_test.exs
@@ -0,0 +1,21 @@
+defmodule PlugLocale.AcceptLanguageTest do
+  use ExUnit.Case, async: true
+  alias PlugLocale.AcceptLanguage
+
+  describe "extract_locales/1" do
+    test "works for normal languages" do
+      assert AcceptLanguage.extract_locales("en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7") ==
+               ["en-US", "en", "zh-CN", "zh"]
+    end
+
+    test "works for languages with extra spaces" do
+      assert AcceptLanguage.extract_locales("en-US, en;q=0.9, zh-CN;q=0.8, zh;q=0.7") ==
+               ["en-US", "en", "zh-CN", "zh"]
+    end
+
+    test "works for languages in arbitrary order" do
+      assert AcceptLanguage.extract_locales("en-US,zh-CN;q=0.8,zh;q=0.7,en;q=0.9") ==
+               ["en-US", "en", "zh-CN", "zh"]
+    end
+  end
+end
