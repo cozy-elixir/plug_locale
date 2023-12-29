abae8186a97991a80ca972104408b3c716d12f92
c4710n <c4710n@users.noreply.github.com>
2023-12-29 17:00:26 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PlugLocale.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Plugs for putting locale into `assigns` storage."
   @source_url "https://github.com/cozy-elixir/plug_locale"
 
